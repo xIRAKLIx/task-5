f2d63246fdf2781236ac747c9d8dcf417002754c
atuli-gorgodze <atuligorgodze27@gmail.com>
2025-05-01 00:28:59 +0400

    commit

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const session = require("express-session");
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
-const blogsRouter= require('./routes/blogs');
-const logoutRouter= require('./routes/logout');
+const blogsRouter = require('./routes/blogs');
+const logoutRouter = require('./routes/logout');
+
 
 const app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -27,7 +28,7 @@ app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false } // Set secure: true in production with HTTPS
+  cookie: { secure: false }
 }));
 
 
@@ -35,21 +36,21 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
-app.use('/blogs', blogsRouter)
-app.use('/logout', logoutRouter)
+app.use('/blogs', blogsRouter);
+app.use('/logout', logoutRouter);
 
-// catch 404 and forward to error handler
+
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/blogs.js
+++ routes/blogs.js
@@ -6,30 +6,33 @@ const BLOGS_FILE = 'blogs.json';
 
 const requireAuth = (req, res, next) => {
     if (req.session.user) {
-        next()
+        next();
     } else {
         console.log('No user found.');
         res.redirect('/login');
     }
-}
+};
 
 router.get('/', requireAuth, function (req, res, next) {
     const blogs = JSON.parse(fs.readFileSync(BLOGS_FILE));
-
     const email = req.session.user.email;
-
-    res.render('blogs', {blogs, email});
+    res.render('blogs', { blogs, email });
 });
 
 router.get('/new', requireAuth, function (req, res, next) {
-    res.render('new_blog', {error: null});
+    const email = req.session.user.email;
+    res.render('new_blog', { error: null, email });
 });
 
 router.post('/new', requireAuth, function (req, res, next) {
-    const {title, content} = req.body;
+    const { title, content } = req.body;
+    const email = req.session.user.email;
 
     if (!title || !content) {
-        res.render("new_blog", {error: "Missing title or content"});
+        return res.render('new_blog', {
+            error: 'Missing title or content',
+            email
+        });
     }
 
     const blogs = JSON.parse(fs.readFileSync(BLOGS_FILE));
@@ -37,9 +40,9 @@ router.post('/new', requireAuth, function (req, res, next) {
         id: String(Date.now()),
         title,
         content,
-        author: req.session.user.email,
+        author: email,
         date: new Date().toLocaleString()
-    }
+    };
 
     blogs.push(newBlog);
     fs.writeFileSync(BLOGS_FILE, JSON.stringify(blogs, null, 2));
